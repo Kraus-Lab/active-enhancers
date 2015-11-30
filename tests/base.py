36cd71d2e07cca5081381d600efb6e3a59e2a153
Venkat Malladi <Venkat.Malladi@utsouthwestern.edu>
2015-11-30 14:52:48 -0600

    Fixed the output returned.

--- tests/base.py
+++ tests/base.py
@@ -6,4 +6,4 @@ def check_docker_output(tool):
     command = 'docker run ' + tool
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     output = process.communicate()
-    return out
+    return output
