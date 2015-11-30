f5d95978cdb601ce99b3a1b35682c443f66d79e9
Venkat Malladi <Venkat.Malladi@utsouthwestern.edu>
2015-11-30 14:04:04 -0600

    Removed conftest, use base file and import function.

---
+++ tests/base.py
@@ -0,0 +1,9 @@
+import subprocess
+import pytest
+
+
+def check_docker_output(tool):
+    command = 'docker run ' + tool
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = process.communicate()
+    return out
