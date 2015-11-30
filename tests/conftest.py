d88ff8fefff608eac5a70856987cb01265722166
Venkat Malladi <vsmalladi@gmail.com>
2015-11-30 13:18:40 -0600

    Remove fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import subprocess
 import pytest
 
-@pytest.fixture(params=tool)
-def check_docker_output(request):
-    command = 'docker run ' + request.tool
+
+def check_docker_output(tool):
+    command = 'docker run ' + tool
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     output = process.communicate()
     return out
