ae8e39076cd1727d79d36e7d72f2b0c3646c7e82
Venkat Malladi <vsmalladi@gmail.com>
2015-11-30 13:00:13 -0600

    Update fixture to use params

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import subprocess
 import pytest
 
-@pytest.fixture
-def check_docker_output(tool):
-    command = 'docker run ' + tool
+@pytest.fixture(params=tool)
+def check_docker_output(request):
+    command = 'docker run ' + request.tool
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     output = process.communicate()
     return out
