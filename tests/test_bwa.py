c2764f4cbea64def76e2b0baaacbacf6aa3b4969
Venkat Malladi <vsmalladi@gmail.com>
2015-11-30 13:40:11 -0600

    Fix test removing fixtures

--- tests/test_bwa.py
+++ tests/test_bwa.py
@@ -3,6 +3,6 @@ import tempfile
 import pytest
 
 
-def test_bwa_call(check_docker_output):
+def test_bwa_call():
     out, err = check_docker_output(tool='active-enhancers/bwa')
     self.assertTrue('Program: bwa' in out)
