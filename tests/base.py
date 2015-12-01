f30f5eab55e0052f046cd4a8aff5ff239bdd1386
Venkat Malladi <Venkat.Malladi@utsouthwestern.edu>
2015-11-30 18:30:52 -0600

    added build images and space.

--- tests/base.py
+++ tests/base.py
@@ -3,7 +3,7 @@ import pytest
 
 
 def check_docker_output(tool):
-    command = 'docker run active-enhancers' + tool
+    command = 'docker run -rm -ti active-enhancers ' + tool
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     output = process.communicate()
     return output
